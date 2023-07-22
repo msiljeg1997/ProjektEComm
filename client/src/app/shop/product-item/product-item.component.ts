892463ec0f7d584acd3d82af3e3c2fabb10e1eed
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-07-22 10:47:26 +0200

    end of lesson 150

--- client/src/app/shop/product-item/product-item.component.ts
+++ client/src/app/shop/product-item/product-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { iProduct } from '../../shared/models/iProduct';
 import { CommonModule } from '@angular/common';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-item',
@@ -9,5 +10,21 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductItemComponent {
 @Input() product?: iProduct;
+
+
+constructor(private basketService : BasketService) {}
+
+addItemToBasket(){
+  this.product && this.basketService.addItemToBasket(this.product);
+
+}
+
+
+handleButtonClick(event: MouseEvent) {
+  console.log(event);
+}
+
+
+
 
 }
