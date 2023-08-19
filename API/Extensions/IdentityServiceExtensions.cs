d261b1f04b819967f4d9ffd36ae5121c9c09caef
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-08-19 10:54:42 +0200

    end of 169.

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -2,21 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities.Identity;
 using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
     public static class IdentityServiceExtensions
     {
-        public static IServiceCollection AddIdentityServices (this IServiceCollection services, IConfiguration config) 
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<AppIdentityDbContext>(opt =>
             {
                 opt.UseSqlite(config.GetConnectionString("IdentityConnection"));
             });
+
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                // add Identity options here
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>()
+            .AddSignInManager<SignInManager<AppUser>>();
+
+
+            //Authentication uvjek prije Authorization jer nemozemo authorizirati nekoga prije nego sta provjerimo tko je
+            services.AddAuthentication();
+            services.AddAuthorization();
+
+
             return services;
         }
-        
+
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
 using API.Extensions;
 using API.MIddleware;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,9 +28,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseStaticFiles();
-
 app.UseCors("CorsPolicy");
 
+//Authentication uvjek prije Authorization jer nemozemo authorizirati nekoga prije nego sta provjerimo tko je
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
@@ -35,12 +39,16 @@ app.MapControllers();
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 var context = services.GetRequiredService<StoreContext>();
+var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+var userManager = services.GetRequiredService<UserManager<AppUser>>();
 var logger = services.GetRequiredService<ILogger<Program>>();
 
 try
 {
     await context.Database.MigrateAsync();
+    await identityContext.Database.MigrateAsync();
     await StoreContextSeed.SeedAsync(context);
+    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
 
 }
 catch (Exception ex)
