3cab81985caa447d558b982be7fe93cdee448cf7
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-08-21 10:09:07 +0200

    end of 176.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Errors;
+using API.Extensions;
 using Core.Entities.Identity;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +22,39 @@ namespace API.Controllers
             _userManager = userManager;
 
         }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+         
+            var user = await _userManager.FindByEmailFromClaimsPrinciple(User);
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                DisplayName = user.DisplayName
+            };
+        }
+
+
+        [HttpGet("emailexists")]
+        public async Task<ActionResult<bool>> CheckEmailExsistAsync([FromQuery] string email)
+        {
+            return await _userManager.FindByEmailAsync(email) != null;
+        }
+
+        [Authorize]
+        [HttpGet("address")]
+        public async Task<ActionResult<Address>> GetUserAddress()
+        {
+
+            var user = await _userManager.FindUserByClaimsPrincipleWithAdress(User);
+
+            return user.Address;
+        }
+
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
@@ -31,14 +67,15 @@ namespace API.Controllers
             return new UserDto
             {
                 Email = user.Email,
-                Token = _tokenService.CreateToken(user),  
+                Token = _tokenService.CreateToken(user),
                 DisplayName = user.DisplayName
             };
         }
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
-            var user = new AppUser{
+            var user = new AppUser
+            {
                 DisplayName = registerDto.DisplayName,
                 Email = registerDto.Email,
                 UserName = registerDto.Email
@@ -55,5 +92,6 @@ namespace API.Controllers
             };
 
         }
+
     }
 }
