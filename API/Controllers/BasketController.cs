789f9d225ebc4da398930442f97c0b2c86111d52
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-08-21 13:28:31 +0200

    added vaidation for basket

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,3 +1,5 @@
+using API.DTOs;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -7,8 +9,10 @@ namespace API.Controllers
     public class BasketController : BaseAPIControler
     {
         private readonly IBasketRepository _basketRepository;
-        public BasketController(IBasketRepository basketRepository)
+        private readonly IMapper _mapper;
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -23,11 +27,12 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket (CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket (CustomerBasketDto basket)
         {
-            var UpdatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
 
-            return Ok(UpdatedBasket); 
+            return Ok(updatedBasket); 
         }
 
         [HttpDelete]
