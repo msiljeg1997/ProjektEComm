f2809212860765772d2938bf927c53105d7fae64
Matija Šiljeg <115492643+msiljeg1997@users.noreply.github.com>
2023-08-22 10:14:27 +0200

    dodan servis za login reg logout emprov i kraj 188

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { iUser } from '../shared/models/iUser';
+import { HttpClient } from '@angular/common/http';
+import { Route, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<iUser | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+
+
+  login(values: any) {
+    return this.http.post<iUser>(this.baseUrl + 'account/login', values).pipe(
+      map(user => {
+        localStorage.setItem('Token', user.Token);
+        this.currentUserSource.next(user);
+      })
+    )
+  }
+
+  register(values: any) {
+    return this.http.post<iUser>(this.baseUrl + 'account/register', values).pipe(
+      map(user => {
+        localStorage.setItem('Token', user.Token);
+        this.currentUserSource.next(user);
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('Token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string){
+    return this.http.get<boolean>(this.baseUrl+ 'account/emailExists?email=' + email);
+  }
+
+
+
 }
